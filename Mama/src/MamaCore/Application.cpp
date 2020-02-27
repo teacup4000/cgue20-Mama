ad1d6fb4452f75c3f0aaaa2b9b8e22611fbc3b35
Lisa <Lisa@LAPTOP-6J373I03>
2020-02-27 02:32:39 +0100

    Window is shown Window has a color now

--- Mama/src/MamaCore/Application.cpp
+++ Mama/src/MamaCore/Application.cpp
@@ -2,12 +2,15 @@
 #include "Application.h"
 
 #include "Events/WindowEvent.h"
+#include "Log.h"
+
+#include <GLFW/glfw3.h>
 
 namespace Mama {
 
 	Application::Application()
 	{
-
+		m_Window = std::unique_ptr<Window>(Window::Create());
 	}
 
 	Application::~Application()
@@ -17,10 +20,12 @@ namespace Mama {
 
 	void Application::Run() {
 
-		WindowResizeEvent e(1280, 720);
-		MAMA_TRACE(e);
-
-		while (true);
+		while (m_Running)
+		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
+		}
 	}
 
 
--- Mama/src/MamaCore/Application.h
+++ Mama/src/MamaCore/Application.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Core.h"
+#include "Events/Event.h"
+#include "Window.h"
 
 namespace Mama {
 
@@ -11,6 +13,9 @@ namespace Mama {
 
 		void Run();
 	
+	private:
+		std::unique_ptr<Window> m_Window;
+		bool m_Running = true;
 	};
 	Application* CreateApplication();
 }
